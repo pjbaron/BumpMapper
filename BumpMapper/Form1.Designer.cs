2d03dc57ea5234c1c0364199a1b3b60603f04724
Pete Baron <simoleste@gmail.com>
2015-05-03 00:51:29 +1200

    Bump Map creation program in C#.  This program creates bump maps in the Phaser 3 renderer format.  It attempts to create bump maps using Sobel operators in the horizontal and vertical directions separately.  The Red channel of the output image represents the horizontal edges, the Green channel represents the vertical edges.  The auto generated height maps are not very good... TODO: implement an 'Edit Bump' system.

---
+++ BumpMapper/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace BumpMapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox_src = new System.Windows.Forms.PictureBox();
+            this.pictureBox_bump = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox_depth = new System.Windows.Forms.PictureBox();
+            this.buttonLoadBump = new System.Windows.Forms.Button();
+            this.trackBarPower = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBarRadius = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_src)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_bump)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_depth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPower)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRadius)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.trackBarRadius);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.trackBarPower);
+            this.panel1.Controls.Add(this.buttonLoadBump);
+            this.panel1.Controls.Add(this.buttonEdit);
+            this.panel1.Controls.Add(this.buttonCreate);
+            this.panel1.Controls.Add(this.buttonSave);
+            this.panel1.Controls.Add(this.buttonLoad);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(721, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(234, 37);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(93, 23);
+            this.buttonEdit.TabIndex = 4;
+            this.buttonEdit.Text = "Edit Bumps";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(333, 8);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(93, 23);
+            this.buttonCreate.TabIndex = 3;
+            this.buttonCreate.Text = "Create Bumps";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(234, 66);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(93, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save Bump...";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(12, 8);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(93, 23);
+            this.buttonLoad.TabIndex = 1;
+            this.buttonLoad.Text = "Load Texture...";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 100);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox_src);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox_bump);
+            this.splitContainer1.Size = new System.Drawing.Size(483, 242);
+            this.splitContainer1.SplitterDistance = 230;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // pictureBox_src
+            // 
+            this.pictureBox_src.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox_src.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_src.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_src.Name = "pictureBox_src";
+            this.pictureBox_src.Size = new System.Drawing.Size(230, 242);
+            this.pictureBox_src.TabIndex = 0;
+            this.pictureBox_src.TabStop = false;
+            // 
+            // pictureBox_bump
+            // 
+            this.pictureBox_bump.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox_bump.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_bump.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_bump.Name = "pictureBox_bump";
+            this.pictureBox_bump.Size = new System.Drawing.Size(249, 242);
+            this.pictureBox_bump.TabIndex = 0;
+            this.pictureBox_bump.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.Controls.Add(this.pictureBox_depth);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(483, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(238, 242);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBox_depth
+            // 
+            this.pictureBox_depth.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox_depth.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox_depth.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_depth.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_depth.Name = "pictureBox_depth";
+            this.pictureBox_depth.Size = new System.Drawing.Size(238, 242);
+            this.pictureBox_depth.TabIndex = 0;
+            this.pictureBox_depth.TabStop = false;
+            this.pictureBox_depth.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_depth_MouseMove);
+            // 
+            // buttonLoadBump
+            // 
+            this.buttonLoadBump.Location = new System.Drawing.Point(234, 8);
+            this.buttonLoadBump.Name = "buttonLoadBump";
+            this.buttonLoadBump.Size = new System.Drawing.Size(93, 23);
+            this.buttonLoadBump.TabIndex = 5;
+            this.buttonLoadBump.Text = "Load Bump...";
+            this.buttonLoadBump.UseVisualStyleBackColor = true;
+            this.buttonLoadBump.Click += new System.EventHandler(this.buttonLoadBump_Click);
+            // 
+            // trackBarPower
+            // 
+            this.trackBarPower.Location = new System.Drawing.Point(570, 8);
+            this.trackBarPower.Maximum = 16;
+            this.trackBarPower.Name = "trackBarPower";
+            this.trackBarPower.Size = new System.Drawing.Size(139, 45);
+            this.trackBarPower.TabIndex = 6;
+            this.trackBarPower.TickFrequency = 4;
+            this.trackBarPower.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBarPower.Scroll += new System.EventHandler(this.trackBarPower_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(498, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Light Power:";
+            // 
+            // trackBarRadius
+            // 
+            this.trackBarRadius.Location = new System.Drawing.Point(570, 49);
+            this.trackBarRadius.Name = "trackBarRadius";
+            this.trackBarRadius.Size = new System.Drawing.Size(139, 45);
+            this.trackBarRadius.TabIndex = 8;
+            this.trackBarRadius.TickFrequency = 2;
+            this.trackBarRadius.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBarRadius.Scroll += new System.EventHandler(this.trackBarRadius_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(498, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Light Radius:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 342);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_src)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_bump)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_depth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPower)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRadius)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox_src;
+        private System.Windows.Forms.PictureBox pictureBox_bump;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox_depth;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonLoadBump;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBarPower;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBarRadius;
+    }
+}
+
---
+++ BumpMapper/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace BumpMapper
+{
+    public partial class Form1 : Form
+    {
+        private Image textureImage;
+
+        private Image filterImage;
+        private BitmapData filterSource;
+        private byte[] filterPixels;
+        private Random rnd;
+        private int lightPower = 6;
+        private double lightRadius = 0.3;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            trackBarPower.Value = lightPower;
+            trackBarRadius.Value = (int)(lightRadius * 10.0 + 0.5);
+        }
+
+
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            DialogResult r = fd.ShowDialog();
+            if (r == DialogResult.OK)
+            {
+                string file = fd.FileName;
+                textureImage = Image.FromFile(file);
+                pictureBox_src.Image = textureImage;
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog fd = new SaveFileDialog();
+            DialogResult r = fd.ShowDialog();
+            if (r == DialogResult.OK)
+            {
+                string file = fd.FileName;
+                filterImage.Save(file);
+            }
+        }
+
+        private void buttonLoadBump_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            DialogResult r = fd.ShowDialog();
+            if (r == DialogResult.OK)
+            {
+                string file = fd.FileName;
+                filterImage = Image.FromFile(file);
+                GetFilterPixels();
+                pictureBox_bump.Image = filterImage;
+            }
+
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            if (textureImage != null)
+            {
+                CreateBumps(textureImage);
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBarPower_Scroll(object sender, EventArgs e)
+        {
+            lightPower = trackBarPower.Value;
+        }
+
+        private void trackBarRadius_Scroll(object sender, EventArgs e)
+        {
+            lightRadius = (double)trackBarRadius.Value * 0.1;
+        }
+
+
+
+        private void CreateBumps(Image textureImage)
+        {
+            Bitmap greyImage = MakeGrayscale((Bitmap)textureImage);
+            filterImage = (Image)MakeFilter(greyImage);
+            pictureBox_bump.Image = filterImage;
+            GetFilterPixels();
+            pictureBox_depth.BackgroundImage = (Image)CreateTiles(textureImage, pictureBox_depth.Width, pictureBox_depth.Height);
+        }
+
+        private void GetFilterPixels()
+        {
+            // get the filter pixels for rapid access
+            filterSource = ((Bitmap)filterImage).LockBits(new Rectangle(0, 0, filterImage.Width, filterImage.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            filterPixels = new byte[filterSource.Stride * filterImage.Height];
+            Marshal.Copy(filterSource.Scan0, filterPixels, 0, filterPixels.Length);
+            ((Bitmap)filterImage).UnlockBits(filterSource);
+        }
+
+        private Bitmap CreateTiles(Image tile, int width, int height)
+        {
+            Bitmap img = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage((Image)img);
+
+            for (var y = 0; y < height; y += tile.Height)
+                for (var x = 0; x < width; x += tile.Width)
+                    g.DrawImage(tile, x, y);
+            g.Dispose();
+            return img;
+        }
+
+        private int Clamp(int value, int min, int max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
+
+        private Bitmap MakeFilter(Bitmap src)
+        {
+            Bitmap newBitmap = new Bitmap(src.Width, src.Height);
+            Graphics grfx = Graphics.FromImage(src);
+
+            // get the pixels
+            BitmapData sourceData = src.LockBits(new Rectangle (0, 0, src.Width, src.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb); 
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height]; 
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length); 
+            src.UnlockBits(sourceData);
+
+            byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+            for (int c = 0; c < resultBuffer.Length; c++)
+                resultBuffer[c] = 0;
+            
+            int[,] filterx = new int[3, 3] { { 1, 0, -1 }, { 2, 0, -2 }, { 1, 0, -1 } };
+            int[,] filtery = new int[3, 3] { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
+            int[][,] filters = new int[2][,] { filterx, filtery };
+
+            int filterWidth = 3; 
+            int filterHeight = 3;
+            int filterHalfWide = (filterWidth - 1) / 2;
+            int filterHalfHigh = (filterHeight - 1) / 2;
+
+            System.Drawing.Color[] colors = { Color.FromArgb(255, 0, 0), Color.FromArgb(0, 255, 0) };
+
+            for (int i = 0; i < filters.Length; i++)
+            {
+                int[,] filter = filters[i];
+                int rmul = (int)(colors[i].R / 255);
+                int gmul = (int)(colors[i].G / 255);
+                int srcPos = 0;
+
+                // for every pixel of the image
+                for (int y = filterHalfHigh; y < src.Height - filterHalfHigh; y++)
+                {
+                    for (int x = filterHalfWide; x < src.Width - filterHalfWide; x++)
+                    {
+                        int r = 128 * rmul, g = 128 * gmul, b = 0;
+                        srcPos = x * 4 + y * sourceData.Stride;
+
+                        // for every value in the filter
+                        for (int fy = -filterHalfHigh; fy <= filterHalfHigh; fy++)
+                        {
+                            for (int fx = -filterHalfWide; fx <= filterHalfWide; fx++)
+                            {
+                                int f = filter[fy + filterHalfHigh, fx + filterHalfWide];
+                                int off = srcPos + fx * 4 + fy * sourceData.Stride;
+                                r += pixelBuffer[off + 2] * f * rmul;
+                                g += pixelBuffer[off + 1] * f * gmul;
+                            }
+                        }
+
+                        resultBuffer[srcPos + 2] += (byte)(Clamp(r, 0, 255));
+                        resultBuffer[srcPos + 1] += (byte)(Clamp(g, 0, 255));
+                        resultBuffer[srcPos + 0] += (byte)(Clamp(b, 0, 255));
+                        resultBuffer[srcPos + 3] = 255;
+                    }
+                }
+            }
+
+            // create bitmap to hold results
+            Bitmap resultBitmap = new Bitmap(src.Width, src.Height);
+            BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+            resultBitmap.UnlockBits(resultData);
+            return resultBitmap; 
+        }
+
+
+        private Bitmap MakeGrayscale(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][] 
+                {
+                    new float[] {.3f, .3f, .3f, 0, 0},
+                    new float[] {.59f, .59f, .59f, 0, 0},
+                    new float[] {.11f, .11f, .11f, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {0, 0, 0, 0, 1}
+                });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
+
+        private void pictureBox_depth_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (textureImage != null && filterImage != null)
+            {
+                pictureBox_depth.BackgroundImage = showLighting(e.Location.X, e.Location.Y);
+                // force the picture to refresh immediately
+                // there's probably another mousemove event ready to jump in already and that makes the visual update steppy
+                pictureBox_depth.Refresh();
+                System.Threading.Thread.Sleep(0);
+            }
+        }
+
+        private Bitmap showLighting(int mx, int my)
+        {
+            Bitmap src = CreateTiles(textureImage, pictureBox_depth.Width, pictureBox_depth.Height);
+
+            // get the pixels
+            BitmapData sourceData = src.LockBits(new Rectangle(0, 0, src.Width, src.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            src.UnlockBits(sourceData);
+
+            // create the output pixel buffer
+            byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            int srcPos = 0;
+
+            // for every pixel of the image
+            for (int y = 0; y < src.Height; y++)
+            {
+                for (int x = 0; x < src.Width; x++)
+                {
+                    srcPos = x * 4 + y * sourceData.Stride;
+
+                    double r = (double)pixelBuffer[srcPos + 2] / 255.0;
+                    double g = (double)pixelBuffer[srcPos + 1] / 255.0;
+                    double b = (double)pixelBuffer[srcPos + 0] / 255.0;
+
+                    double red, green, blue;
+
+                    ApplyLighting(
+                        (double)x / (double)pictureBox_depth.Width, (double)y / (double)pictureBox_depth.Height,
+                        (double)mx / (double)pictureBox_depth.Width, (double)my / (double)pictureBox_depth.Height,
+                        out red, out green, out blue);
+
+                    resultBuffer[srcPos + 2] = (byte)(Clamp((int)((r + red) * 255.0), 0, 255));
+                    resultBuffer[srcPos + 1] = (byte)(Clamp((int)((g + green) * 255.0), 0, 255));
+                    resultBuffer[srcPos + 0] = (byte)(Clamp((int)((b + blue) * 255.0), 0, 255));
+                    resultBuffer[srcPos + 3] = 255;
+                }
+            }
+
+            // create bitmap to hold results
+            Bitmap resultBitmap = new Bitmap(src.Width, src.Height);
+            BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+            resultBitmap.UnlockBits(resultData);
+
+            return resultBitmap;
+        }
+
+
+        // emulate the shader
+        void ApplyLighting(double px, double py, double mx, double my, out double r, out double g, out double b)
+        {
+            Color brightness = Color.FromArgb(lightPower, lightPower, lightPower);
+
+            // make the light edges flicker a little to create a softer blur effect
+            //double dx = (mx - px) + (rnd.NextDouble() - 0.5) * 0.01;
+            //double dy = (my - py) + (rnd.NextDouble() - 0.5) * 0.01;
+            // hard stationary light edges
+            double dx = (mx - px);
+            double dy = (my - py);
+
+            double d = Math.Sqrt(dx * dx + dy * dy) / lightRadius;
+            if (d >= 1.0)
+            {
+                r = g = b = 0.0;
+                return;
+            }
+
+            double od = d * 3.0 + 0.3;
+            double bright = GetBright(px, py, dx, dy, 1.0 - d);
+            double mul = (0.09 / (od * od)) * bright;
+            r = (double)brightness.R * mul;
+            g = (double)brightness.G * mul;
+            b = (double)brightness.B * mul;
+        }
+
+
+        double GetBright(double px, double py, double dx, double dy, double id)
+        {
+            double ret = 0.0;
+
+            int sx = (int)(px * pictureBox_depth.Width) % filterImage.Width;
+            int sy = (int)(py * pictureBox_depth.Height) % filterImage.Height;
+
+            int srcPos = sx * 4 + sy * filterSource.Stride;
+            double r = ((double)filterPixels[srcPos + 2] / 255.0) - 0.5;
+            double g = ((double)filterPixels[srcPos + 1] / 255.0) - 0.5;
+
+            if (Math.Abs(r) <= 0.1)
+                ret += 0.5 * id;
+            else
+            {
+                if (dx < 0.0 && r < 0.0) ret += Math.Abs(r) * id;
+                if (dx > 0.0 && r > 0.0) ret += Math.Abs(r) * id;
+            }
+
+            if (Math.Abs(g) <= 0.1)
+                ret += 0.5 * id;
+            else
+            {
+                if (dy > 0.0 && g < 0.0) ret += Math.Abs(g) * id;
+                if (dy < 0.0 && g > 0.0) ret += Math.Abs(g) * id;
+            }
+
+            return ret;
+        }
+
+
+    }
+}
